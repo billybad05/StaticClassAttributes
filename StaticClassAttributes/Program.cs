a2e6b1e4059800fb69a78fa7850698000bd8f809
Billy Dotson <billybad5@gmail.com>
2022-10-14 16:07:45 -0400

    Add project files.

---
+++ StaticClassAttributes/Program.cs
@@ -0,0 +1,10 @@
+﻿using StaticClassAttributes;
+
+Console.WriteLine("StaticClassAttributes");
+
+Song holiday = new Song("Holiday", "Green Day", 200);
+Console.WriteLine(Song.songCount);
+Song kashmir = new Song("Kashmir", "Led Zeppelin", 150);
+Console.WriteLine(Song.songCount);
+
+Console.WriteLine(kashmir.getSongCount());
